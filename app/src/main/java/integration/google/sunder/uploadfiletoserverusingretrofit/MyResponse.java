79b3fa5d9ba7aa380585f3d0d52de8bee5ac7ebe
Sunder <sunderrra31@gmail.com>
2018-05-01 16:58:34 +0530

    Initial commit

---
+++ app/src/main/java/integration/google/sunder/uploadfiletoserverusingretrofit/MyResponse.java
@@ -0,0 +1,10 @@
+package integration.google.sunder.uploadfiletoserverusingretrofit;
+
+/**
+ * Created by Belal on 10/5/2017.
+ */
+
+public class MyResponse {
+    boolean error;
+    String message;
+}
